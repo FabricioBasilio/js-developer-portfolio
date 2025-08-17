8cf47eb589728caa0b4bcf191c3f9b801db40dc0
Fabricio Basilio <fabriciobasilioo@gmail.com>
2025-08-17 16:51:47 -0300

    feat: adição de softskills à página

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,46 +1,36 @@
-
 function updateProfileInfo(profileData) {
-    const profilePhoto = document.getElementById("profile_photo")
-    const profileName = document.getElementById("profile_name")
-    const profileJob = document.getElementById("profile_job")
-    const profileLocation = document.getElementById("profile_location")
-    const profilePhone = document.getElementById("profile_phone")
-    const profileEmail = document.getElementById("profile_email")
+  const profilePhoto = document.getElementById("profile_photo");
+  const profileName = document.getElementById("profile_name");
+  const profileJob = document.getElementById("profile_job");
+  const profileLocation = document.getElementById("profile_location");
+  const profilePhone = document.getElementById("profile_phone");
+  const profileEmail = document.getElementById("profile_email");
 
+  profilePhoto.setAttribute("src", profileData.foto_perfil);
+  profilePhoto.setAttribute("alt", profileData.nome);
 
+  profileName.innerHTML = profileData.nome;
 
-    profilePhoto.setAttribute("src", profileData.foto_perfil)
-    profilePhoto.setAttribute("alt", profileData.nome)
+  profileJob.innerHTML = profileData.titulo;
 
-    profileName.innerHTML = profileData.nome;
+  profileLocation.innerHTML = profileData.localidade.estado;
 
-    profileJob.innerHTML = profileData.titulo;
+  profilePhone.innerHTML = profileData.telefone;
+  profilePhone.setAttribute("href", "tel:(96) 98104-8077");
 
-    profileLocation.innerHTML = profileData.localidade.estado;
-    profilePhone.innerHTML = profileData.telefone
-    profilePhone.setAttribute("href", "tel:(96) 98104-8077")
-
-    profileEmail.innerHTML = profileData.email
-    profileEmail.setAttribute("href", "mailto:fabriciobasilioo@gmail.com")
-
-
-    
-
+  profileEmail.innerHTML = profileData.email;
+  profileEmail.setAttribute("href", "mailto:fabriciobasilioo@gmail.com");
 }
 
+function updateSoftSkills(softSkills) {
+  const softSkillsList = document.getElementById("profile_soft_skills");
+  softSkillsList.innerHTML = softSkills.map((skill) => `<li>${skill}</li>`).join("");
+}
 
 (async () => {
-    const profileData = await fetchProfileData()
-    console.log(profileData);
+  const profileData = await fetchProfileData();
+  console.log(profileData);
 
-    updateProfileInfo(profileData)
-
-
-
-
-
-
-
-
-
+  updateProfileInfo(profileData);
+  updateSoftSkills(profileData.skills.softSkills);
 })();
