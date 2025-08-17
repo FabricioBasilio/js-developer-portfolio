e48c0584e5dde90e28d20da3e243dac67e15173f
Fabricio Basilio <fabriciobasilioo@gmail.com>
2025-08-17 16:33:37 -0300

    feat: adição dos dados principais na página e adição de foto de perfil aos dados

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,6 @@
 async function fetchProfileData() {
     const url = "https://raw.githubusercontent.com/FabricioBasilio/js-developer-portfolio/refs/heads/projeto-base/data/profile.json"
     const data = await fetch(url).then(data => data.json());
-    console.log(data);
     
     return data;
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,46 @@
+
+function updateProfileInfo(profileData) {
+    const profilePhoto = document.getElementById("profile_photo")
+    const profileName = document.getElementById("profile_name")
+    const profileJob = document.getElementById("profile_job")
+    const profileLocation = document.getElementById("profile_location")
+    const profilePhone = document.getElementById("profile_phone")
+    const profileEmail = document.getElementById("profile_email")
+
+
+
+    profilePhoto.setAttribute("src", profileData.foto_perfil)
+    profilePhoto.setAttribute("alt", profileData.nome)
+
+    profileName.innerHTML = profileData.nome;
+
+    profileJob.innerHTML = profileData.titulo;
+
+    profileLocation.innerHTML = profileData.localidade.estado;
+    profilePhone.innerHTML = profileData.telefone
+    profilePhone.setAttribute("href", "tel:(96) 98104-8077")
+
+    profileEmail.innerHTML = profileData.email
+    profileEmail.setAttribute("href", "mailto:fabriciobasilioo@gmail.com")
+
+
+    
+
+}
+
+
 (async () => {
     const profileData = await fetchProfileData()
+    console.log(profileData);
+
+    updateProfileInfo(profileData)
+
+
+
+
+
+
+
+
+
 })();
